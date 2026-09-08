fe1b7c0131ac889cd112cefc3536f703fd2ad237
v9x <59032172+V9X@users.noreply.github.com>
2026-09-08 23:14:53 +0200

    add keyd

---
+++ modules/aspects/machine/keyboard.nix
@@ -0,0 +1,24 @@
+{ globals, ... }: {
+  flake.modules.nixos.keyboard = {
+    services.keyd = {
+      enable = true;
+
+      keyboards.default = {
+        ids = [ "*" ];
+
+        settings = {
+          global.overload_tap_timeout = 200;
+
+          main.capslock = "overload(nav, esc)";
+
+          nav = {
+            "${globals.keys.left}" = "left";
+            "${globals.keys.down}" = "down";
+            "${globals.keys.up}" = "up";
+            "${globals.keys.right}" = "right";
+          };
+        };
+      };
+    };
+  };
+}
--- modules/hosts/base.nix
+++ modules/hosts/base.nix
@@ -14,6 +14,7 @@
       thunderbolt
 
       fonts
+      keyboard
       locale
       network
       removable
