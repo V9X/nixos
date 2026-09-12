fafc631ca076c7413726bf2c44ccb8484124df62
v9x <59032172+V9X@users.noreply.github.com>
2026-09-12 22:48:31 +0200

    update amd: enable graphics

--- modules/aspects/machine/hardware/amd.nix
+++ modules/aspects/machine/hardware/amd.nix
@@ -2,6 +2,7 @@
   flake.modules.nixos.amd = {
     boot.kernelModules = [ "kvm-amd" ];
     hardware.cpu.amd.updateMicrocode = true;
+    hardware.graphics.enable = true;
     hardware.amdgpu.initrd.enable = true;
   };
 }
